9c7c7c1c7e27b4aabfd50acee1f253a272129ee2
fernrey <fernando39@live.com>
2022-03-28 15:20:45 -0400

    new chnges

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "../App.js"
+import "../Styles/forum.css";
 export default function Contact(){
 	return (
 		<div className="Home">
 		<h1>Contact us!</h1>
-		
-
-
-
+		   <div class="forum">
+        <form>
+            <label for="Username"> Username </label><br/>
+            <input type ="text" id="user"/><br/>
+            <label for="Email">Email </label><br/>
+            <input type ="text" id="email"/><br/>
+            <label for="Password"> Password</label><br/>
+            <input type ="text" id="pass"/><br/>
+            <input type="submit" className="submit"/><br/>
+        </form>
+         </div>
 		</div>
 		)
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,16 +4,20 @@ import { Link } from "react-router-dom";
 
 import "../Style.css";
 import "../Styles/Navbar.css";
+
 export default function Navbar(){
 	return (
-		<div className="Nav">
-		<nav className="Navbar">
-       <Link to="/" className="home"> ReyesApplianceRepair</Link>
-       <Link to="about" className="about"> About </Link>
-       <Link to="pricing" className="pricing"> Pricing </Link>
-       <Link to="contact" className="contact"> Contact </Link>
-       </nav>
-       </div>
+	<div className="Nav">
+        <div className="MainTitle"> ReyesApplianceRepair </div>
+	   <nav className="Navbar">
+        <div>
+        <Link to="/" className="home"> ReyesApplianceRepair</Link>
+        </div>
+        <Link to="about" className="about"> About </Link>
+        <Link to="pricing" className="pricing"> Pricing </Link>
+        <Link to="contact" className="contact"> Contact </Link>
+        </nav>
+    </div>
        )
 
 
